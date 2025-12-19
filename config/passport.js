2803145debf47273b69059b7d90ab12697dcc041
Charan-vamshi <saicharanreddy344@gmail.com>
2025-12-19 12:34:36 +0530

    Fix OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://formify-production.up.railway.app/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     return cb(null, profile);
